043de1f11b86e2075b8448d36243177023d43654
Alex Barry <aostreetart9@gmail.com>
2019-06-23 01:37:47 -0400

    Update project to generate OS events rather than MIDI messages

--- README.md
+++ README.md
@@ -1,48 +1,27 @@
 # Harmonia
 
-Harmonia is a Python service which can transform MIDI notes en-route.
+Harmonia is a Python application which can transform MIDI notes into native
+keyboard events.
+
+This project was born from my frustration at my inability to use my MIDI
+equipment to control most programs that I use on a daily basis.  Now, your
+MIDI equipment can control anything, without needing to rely on in-application
+integrations.
+
+## Configuration
+
+Harmonia is largely reliant on it's configuration file to function correctly.
 
 ## Dependencies
 
-    pip install mido
+For Linux users:
 
-    pip install redis (optional)
+    pip install --user python-rtmidi mido python3-xlib pillow pyautogui
 
-## Use
+For OSX users:
 
-Harmonia requires a configuration file in order to execute.  Here's the example:
+    pip install python-rtmidi mido pyobjc-core pyobjc pyautogui
 
-    [Logging]
-    File: harmonia.log
-    Level: Debug
+For Windows users:
 
-    [Connection]
-    Input: loopMIDI
-    Output: loopMIDI
-
-    [Redis]
-    Host: localhost
-    Port: 6379
-    DB: 0
-
-    [Notes]
-    NoteMap: {Notes:  [
-                        {
-                          Input: {Type: 'note_on', Note: 60},
-                          Output: {Type: 'note_off', Note: 60}
-                        },
-                        {
-                          Input: {Type: 'note_off', Note: 60}, 
-                          Output: {Type: 'note_on', Note: 60}
-                        }
-                      ]
-                    }
-
-The entries in the Logging section control Harmonia's logging.
-
-The entries in the Connection section determine what MIDI connection Harmonia recieves from & sends to.
-
-The entries in the Redis section can be populated to use a Redis server for persistence, or left blank to have an in-memory cache.
-
-The Notes section should contain a NoteMap which is in JSON format.  See the format above, which shows an example of the off/on notes being switched.
-
+    pip.exe install python-rtmidi mido pyautogui
--- harmonia.py
+++ harmonia.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 11 14:46:33 2017
+The MIT License (MIT)
 
-@author: alex
+Copyright (c) 2017 Alex Barry
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+@author: Alex Barry
 """
 
 # Standard Python Libraries
-import ConfigParser
 import sys
 import json
 import logging
@@ -14,161 +33,91 @@ import logging
 # MIDI Connectivity
 import mido
 
-# Redis Connectivity
-kill_redis = False
-try:
-    import redis
-except Exception as e:
-    print('Redis Driver not found, killing redis connections')
-    kill_redis = True
+# OS Event generation
+import pyautogui
 
-# Cache which abstracts Redis vs Dict
-class Cache(object):
-    
-    def __init__(self, cache_type, host, port, db):
-        self.ctype = cache_type
-        self.chost = host
-        self.cport = port
-        self.cdb = db
-        
-        self.redis_connection = None
-        self.internal_cache = None
-        
-        if self.ctype == 'redis':
-            self.redis_connection = redis.StrictRedis(host=self.chost, port=self.cport, db=self.cdb)
-        elif self.cytpe == 'dict':
-            self.internal_cache = {}
-        else:
-            log.error('Unknown Cache Type')
-        
-    def set_value(self, key, val):
-        if self.ctype == 'redis':
-            self.redis_connection.set(key, val)
-        elif self.cytpe == 'dict':
-            self.internal_cache[key] = val
-    
-    def get_value(self, key):
-        if self.ctype == 'redis':
-            return self.redis_connection.get(key)
-        elif self.cytpe == 'dict':
-            return self.internal_cache[key]
-    
-    def exists(self, key):
-        if self.ctype == 'redis':
-            return self.redis_connection.exists(key)
-        elif self.cytpe == 'dict':
-            if key in self.internal_cache:
-                return True
-            else:
-                return False
+# Filter an input value
+def filter_input(type, value, config_note):
+    if 'value' in config_note:
+        if config_note["value"] != value:
+            return False
+    if config_note["type"] != type:
+        return False
+    return True
 
-#Set up the file logging config
-def config_logging(log_file, log_level):
+# Generate an OS Event based on the configured option
+def generate_os_event(config_note, value):
+    logging.debug("Generating Event for configuration: %s", config_note)
+
+    if config_note["action"]["type"] == "keyboard":
+        # Generate a keyboard event with modifiers
+        for key in config_note["action"]["modifiers"]:
+            # Press down modifier keys
+            pyautogui.keyDown(key)
+        pyautogui.press(config_note["action"]["key"])
+        for key in config_note["action"]["modifiers"]:
+            # Release modifier keys
+            pyautogui.keyUp(key)
+
+# Set up the file logging config
+def config_logging(log_level):
+    FORMAT = '%(asctime)-15s %(message)s'
     if log_level == 'Debug':
-        logging.basicConfig(filename=log_file, level=logging.DEBUG)
+        logging.basicConfig(format=FORMAT, level=logging.DEBUG)
     elif log_level == 'Info':
-        logging.basicConfig(filename=log_file, level=logging.INFO)
+        logging.basicConfig(format=FORMAT, level=logging.INFO)
     elif log_level == 'Warning':
-        logging.basicConfig(filename=log_file, level=logging.WARNING)
+        logging.basicConfig(format=FORMAT, level=logging.WARNING)
     elif log_level == 'Error':
-        logging.basicConfig(filename=log_file, level=logging.ERROR)
+        logging.basicConfig(format=FORMAT, level=logging.ERROR)
     else:
         print("Log level not set to one of the given options, defaulting to debug level")
-        logging.basicConfig(filename=log_file, level=logging.DEBUG)
+        logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 def execute_main(config_file):
-    
-    global kill_redis
-    
-    # Read the configuration file
-    config = ConfigParser.ConfigParser()
-    config.read(config_file)
-    
-    # Pull Configuration values from the ini file
-    inp_midi_name = config.get('Connection', 'Input')
-    out_midi_name = config.get('Connection', 'Output')
-    redis_host = config.get('Redis', 'Host')
-    redis_port = config.get('Redis', 'Port')
-    redis_db = config.get('Redis', 'DB')
-    note_mapping = config.get('Notes', 'NoteMap')
-    log_file = config.get('Logging', 'File')
-    log_level = config.get('Logging', 'Level')
-    
-    # Configure Logging
-    config_logging(log_file, log_level)
-    
-    # Connect to Redis
-    cache = None
-    if kill_redis or ((redis_host is None or redis_host = "") and (redis_port is None or redis_port = "") and (redis_db is None or redis_db = "")):
-        cache = Cache('dict', None, None, None)
-    else:
-        try:
-            cache = Cache('redis', redis_host, redis_port, redis_db)
-        except Exception as e:
-            logging.error('Error connecting to Cache')
-            logging.error(e)
-            sys.exit(-1)
+    conf_json = None
+    with open(config_file, 'r') as cfile:
+        conf_json = json.load(cfile)
 
-    # Parse the Note Map
-    try:
-        note_map = json.loads(note_mapping)
-    except Exception as e:
-        logging.error('Error parsing Note Map')
-        logging.error(e)
-        sys.exit(-1)
-    note_list = note_map['Notes']
-    for note in note_list:
-        cache.set_value('%s-%s' % (note['Input']['Type'], note['Input']['Note']), '%s-%s' % (note['Output']['Type'], note['Output']['Note']))
-    
+    config_logging(conf_json["log_level"])
+    logging.info("Starting Midi Server")
+
     inp_list = mido.get_input_names()
     logging.debug(inp_list)
-    
+
     # Find the correct input MIDI Port
     for name in inp_list:
-        if name.find(inp_midi_name) != -1:
-    
+        if name.find(conf_json["device"]["name"]) != -1:
+
             # Open up the input MIDI port
-            logging.info('Using input Midi Port: %s' % name)
+            logging.info('Using input Midi Port: %s', name)
             inport = mido.open_input(name)
-            
+
             # Recieve inbound MIDI messages
             while True:
                 msg = inport.receive()
                 if msg.type != 'control_change':
-                    logging.info('Message Recieved, Type: %s, Note: %s, Velocity: %s' % (msg.type, msg.note, msg.velocity))
-                    inp_type = msg.type
-                    inp_note = msg.note
-                    # Check if the message type & note exist in Redis
-                    if cache.exists('%s-%s' % (inp_type, inp_note)):
-                        
-                        # Pull the new type and note out of redis
-                        new_msg_attr = cache.get_value('%s-%s' % (inp_type, inp_note))
-                        logging.debug('Object found in Cache: %s' % new_msg_attr)                        
-                        dash_pos = new_msg_attr.rfind('-')
-                        new_msg_type = new_msg_attr[0:dash_pos-1]
-                        new_msg_note = new_msg_attr[dash_pos+1:len(new_msg_attr)]
-                        
-                        # Update the message based on the values returned from redis
-                        msg.type = new_msg_type
-                        msg.note = new_msg_note
-                        
-                    # Send the outbound message
-                    
-                    # Get a list of output ports
-                    out_list = mido.get_output_names()
-                    logging.debug(out_list)
-                    
-                    # Find the first loop MIDI port 
-                    for out_name in out_list:
-                        if out_name.find(out_midi_name) != -1:
-                            outport = mido.open_output(out_name)
-                            outport.send(msg)
-                            logging.debug('Message sent on port %s' % out_name)
-            else:
-                logging.debug('Control change recieved')
-                
+                    logging.debug('Message Recieved, Type: %s, Note: %s, Velocity: %s', msg.type, msg.note, msg.velocity)
+
+                    # Filter the message
+                    if str(msg.note) in conf_json["device"]["notes"]:
+                        config_note = conf_json["device"]["notes"][str(msg.note)]
+                        if filter_input(msg.type, msg.velocity, config_note):
+                            # Execute the configured action
+                            generate_os_event(config_note, msg.velocity)
+
+                else:
+                    logging.debug('Control change recieved, Control ID: %s -- Value: %s', msg.control, msg.value)
+
+                    # Filter the message
+                    if str(msg.control) in conf_json["device"]["notes"]:
+                        config_note = conf_json["device"]["notes"][str(msg.note)]
+                        if filter_input(msg.type, msg.value, config_note):
+                            # Execute the configured action
+                            generate_os_event(config_note, msg.value)
+
             return 0
-            
+
     return -1
 
 if __name__ == "__main__":
